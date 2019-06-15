373a8fdebef2734fc95e4614ffee411ff2c857c4
James <conjam@umich.edu>
2019-06-15 14:48:33 -0400

    PPU background generating logic should maybe work

---
+++ verilog/PALROM.v
@@ -0,0 +1,217 @@
+//Quick ROM spun up using gates; should use ROM on FPGA, but I was having some fun with vim+python scripting this. 
+
+
+module  PALROM(
+                input [5:0] pal_in,
+                
+                
+                output [7:0] red,
+                output [7:0] green,
+                output [7:0] blue
+                
+                
+                 
+                
+                );
+
+    
+
+    assign red =  pal_in == 'd0 ? 'd96 :
+                  pal_in == 'd1 ? 'd0 :
+                  pal_in == 'd2 ? 'd0 :
+                  pal_in == 'd3 ? 'd60 :
+                  pal_in == 'd4 ? 'd100 :
+                  pal_in == 'd5 ? 'd100 :
+                  pal_in == 'd6 ? 'd100 :
+                  pal_in == 'd7 ? 'd81 :
+                  pal_in == 'd8 ? 'd36 :
+                  pal_in == 'd9 ? 'd28 :
+                  pal_in == 'd10 ? 'd0 :
+                  pal_in == 'd11 ? 'd0 :
+                  pal_in == 'd12 ? 'd0 :
+                  pal_in == 'd13 ? 'd0 :
+                  pal_in == 'd14 ? 'd20 :
+                  pal_in == 'd15 ? 'd20 :
+                  pal_in == 'd16 ? 'd174 :
+                  pal_in == 'd17 ? 'd36 :
+                  pal_in == 'd18 ? 'd52 :
+                  pal_in == 'd19 ? 'd116 :
+                  pal_in == 'd20 ? 'd180 :
+                  pal_in == 'd21 ? 'd180 :
+                  pal_in == 'd22 ? 'd180 :
+                  pal_in == 'd23 ? 'd136 :
+                  pal_in == 'd24 ? 'd92 :
+                  pal_in == 'd25 ? 'd56 :
+                  pal_in == 'd26 ? 'd0 :
+                  pal_in == 'd27 ? 'd0 :
+                  pal_in == 'd28 ? 'd0 :
+                  pal_in == 'd29 ? 'd48 :
+                  pal_in == 'd30 ? 'd20 :
+                  pal_in == 'd31 ? 'd20 :
+                  pal_in == 'd32 ? 'd255 :
+                  pal_in == 'd33 ? 'd88 :
+                  pal_in == 'd34 ? 'd132 :
+                  pal_in == 'd35 ? 'd184 :
+                  pal_in == 'd36 ? 'd236 :
+                  pal_in == 'd37 ? 'd248 :
+                  pal_in == 'd38 ? 'd255 :
+                  pal_in == 'd39 ? 'd222 :
+                  pal_in == 'd40 ? 'd183 :
+                  pal_in == 'd41 ? 'd122 :
+                  pal_in == 'd42 ? 'd60 :
+                  pal_in == 'd43 ? 'd52 :
+                  pal_in == 'd44 ? 'd44 :
+                  pal_in == 'd45 ? 'd76 :
+                  pal_in == 'd46 ? 'd20 :
+                  pal_in == 'd47 ? 'd20 :
+                  pal_in == 'd48 ? 'd255 :
+                  pal_in == 'd49 ? 'd192 :
+                  pal_in == 'd50 ? 'd204 :
+                  pal_in == 'd51 ? 'd228 :
+                  pal_in == 'd52 ? 'd252 :
+                  pal_in == 'd53 ? 'd255 :
+                  pal_in == 'd54 ? 'd255 :
+                  pal_in == 'd55 ? 'd244 :
+                  pal_in == 'd56 ? 'd228 :
+                  pal_in == 'd57 ? 'd204 :
+                  pal_in == 'd58 ? 'd180 :
+                  pal_in == 'd59 ? 'd180 :
+                  pal_in == 'd60 ? 'd180 :
+                  pal_in == 'd61 ? 'd182 :
+                  pal_in == 'd62 ? 'd20 :
+                  pal_in == 'd63 ? 'd20 : 0;
+    
+    
+    assign green =  pal_in == 'd0 ? 'd96 :   
+                    pal_in == 'd1 ? 'd44 :
+                    pal_in == 'd2 ? 'd0 :
+                    pal_in == 'd3 ? 'd0 :
+                    pal_in == 'd4 ? 'd0 :
+                    pal_in == 'd5 ? 'd0 :
+                    pal_in == 'd6 ? 'd0 :
+                    pal_in == 'd7 ? 'd24 :
+                    pal_in == 'd8 ? 'd36 :
+                    pal_in == 'd9 ? 'd52 :
+                    pal_in == 'd10 ? 'd68 :
+                    pal_in == 'd11 ? 'd68 :
+                    pal_in == 'd12 ? 'd68 :
+                    pal_in == 'd13 ? 'd0 :
+                    pal_in == 'd14 ? 'd20 :
+                    pal_in == 'd15 ? 'd20 :
+                    pal_in == 'd16 ? 'd174 :
+                    pal_in == 'd17 ? 'd88 :
+                    pal_in == 'd18 ? 'd52 :
+                    pal_in == 'd19 ? 'd36 :
+                    pal_in == 'd20 ? 'd0 :
+                    pal_in == 'd21 ? 'd24 :
+                    pal_in == 'd22 ? 'd28 :
+                    pal_in == 'd23 ? 'd60 :
+                    pal_in == 'd24 ? 'd92 :
+                    pal_in == 'd25 ? 'd108 :
+                    pal_in == 'd26 ? 'd124 :
+                    pal_in == 'd27 ? 'd124 :
+                    pal_in == 'd28 ? 'd124 :
+                    pal_in == 'd29 ? 'd48 :
+                    pal_in == 'd30 ? 'd20 :
+                    pal_in == 'd31 ? 'd20 :
+                    pal_in == 'd32 ? 'd255 :
+                    pal_in == 'd33 ? 'd160 :
+                    pal_in == 'd34 ? 'd132 :
+                    pal_in == 'd35 ? 'd116 :
+                    pal_in == 'd36 ? 'd100 :
+                    pal_in == 'd37 ? 'd108 :
+                    pal_in == 'd38 ? 'd116 :
+                    pal_in == 'd39 ? 'd150 :
+                    pal_in == 'd40 ? 'd183 :
+                    pal_in == 'd41 ? 'd198 :
+                    pal_in == 'd42 ? 'd212 :
+                    pal_in == 'd43 ? 'd200 :
+                    pal_in == 'd44 ? 'd188 :
+                    pal_in == 'd45 ? 'd76 :
+                    pal_in == 'd46 ? 'd20 :
+                    pal_in == 'd47 ? 'd20 :
+                    pal_in == 'd48 ? 'd255 :
+                    pal_in == 'd49 ? 'd216 :
+                    pal_in == 'd50 ? 'd204 :
+                    pal_in == 'd51 ? 'd200 :
+                    pal_in == 'd52 ? 'd196 :
+                    pal_in == 'd53 ? 'd200 :
+                    pal_in == 'd54 ? 'd204 :
+                    pal_in == 'd55 ? 'd216 :
+                    pal_in == 'd56 ? 'd228 :
+                    pal_in == 'd57 ? 'd236 :
+                    pal_in == 'd58 ? 'd244 :
+                    pal_in == 'd59 ? 'd236 :
+                    pal_in == 'd60 ? 'd228 :
+                    pal_in == 'd61 ? 'd182 :
+                    pal_in == 'd62 ? 'd20 :
+                    pal_in == 'd63 ? 'd20 : 0;
+
+    assign blue =  pal_in == 'd0 ? 'd96 :
+                   pal_in == 'd1 ? 'd112 :
+                   pal_in == 'd2 ? 'd156 :
+                   pal_in == 'd3 ? 'd128 :
+                   pal_in == 'd4 ? 'd100 :
+                   pal_in == 'd5 ? 'd60 :
+                   pal_in == 'd6 ? 'd0 :
+                   pal_in == 'd7 ? 'd0 :
+                   pal_in == 'd8 ? 'd0 :
+                   pal_in == 'd9 ? 'd0 :
+                   pal_in == 'd10 ? 'd0 :
+                   pal_in == 'd11 ? 'd44 :
+                   pal_in == 'd12 ? 'd68 :
+                   pal_in == 'd13 ? 'd0 :
+                   pal_in == 'd14 ? 'd20 :
+                   pal_in == 'd15 ? 'd20 :
+                   pal_in == 'd16 ? 'd174 :
+                   pal_in == 'd17 ? 'd184 :
+                   pal_in == 'd18 ? 'd244 :
+                   pal_in == 'd19 ? 'd212 :
+                   pal_in == 'd20 ? 'd180 :
+                   pal_in == 'd21 ? 'd104 :
+                   pal_in == 'd22 ? 'd28 :
+                   pal_in == 'd23 ? 'd24 :
+                   pal_in == 'd24 ? 'd0 :
+                   pal_in == 'd25 ? 'd0 :
+                   pal_in == 'd26 ? 'd0 :
+                   pal_in == 'd27 ? 'd72 :
+                   pal_in == 'd28 ? 'd124 :
+                   pal_in == 'd29 ? 'd48 :
+                   pal_in == 'd30 ? 'd20 :
+                   pal_in == 'd31 ? 'd20 :
+                   pal_in == 'd32 ? 'd255 :
+                   pal_in == 'd33 ? 'd232 :
+                   pal_in == 'd34 ? 'd255 :
+                   pal_in == 'd35 ? 'd255 :
+                   pal_in == 'd36 ? 'd236 :
+                   pal_in == 'd37 ? 'd176 :
+                   pal_in == 'd38 ? 'd116 :
+                   pal_in == 'd39 ? 'd68 :
+                   pal_in == 'd40 ? 'd0 :
+                   pal_in == 'd41 ? 'd40 :
+                   pal_in == 'd42 ? 'd60 :
+                   pal_in == 'd43 ? 'd124 :
+                   pal_in == 'd44 ? 'd188 :
+                   pal_in == 'd45 ? 'd76 :
+                   pal_in == 'd46 ? 'd20 :
+                   pal_in == 'd47 ? 'd20 :
+                   pal_in == 'd48 ? 'd255 :
+                   pal_in == 'd49 ? 'd252 :
+                   pal_in == 'd50 ? 'd255 :
+                   pal_in == 'd51 ? 'd255 :
+                   pal_in == 'd52 ? 'd252 :
+                   pal_in == 'd53 ? 'd228 :
+                   pal_in == 'd54 ? 'd204 :
+                   pal_in == 'd55 ? 'd184 :
+                   pal_in == 'd56 ? 'd164 :
+                   pal_in == 'd57 ? 'd172 :
+                   pal_in == 'd58 ? 'd180 :
+                   pal_in == 'd59 ? 'd204 :
+                   pal_in == 'd60 ? 'd228 :
+                   pal_in == 'd61 ? 'd182 :
+                   pal_in == 'd62 ? 'd20 :
+                   pal_in == 'd63 ? 'd20 : 0;
+
+
+endmodule
+
---
+++ verilog/PPU.v
@@ -0,0 +1,233 @@
+module  PPU(
+                input clock,
+                input reset, 
+                //inputs ... 
+                input [7:0] vram2ppu_data,
+                input [15:0] cpu2ppu_addr,
+                input [7:0]  cpu2ppu_data,
+                input cpu2ppu_wr,
+                
+                //outputs ...  
+                output [15:0] ppu2cpu_addr,
+                output [7:0]  ppu2cpu_data,
+                output [13:0] ppu2vram_addr,
+                output [7:0] ppu2vram_data,
+                output [4:0] pal_index
+                );
+
+    
+    reg [7:0] PPUCTRL, PPUMASK, PPUSTAT, OAMADD, OAMDAT, PPUADD, PPUDAT;
+    reg [7:0] bgA, bgL, atb;
+    reg [15:0] NT_base;
+    wire [15:0] sPT_base, bPT_base;
+    wire [4:0] cy; //the "coarse" y
+    wire [13:0] ptAddr; 
+
+    wire [4:0] bgPalIdx;
+    assign bgPalIdx = {1'b0, attrSR[1][x], attrSR[0][x], bgSR[1][{1'b0,x}], bgSR[0][{1'b0, x}]}; 
+
+    assign pal_index = bgPalIdx;
+
+    reg [1:0] attrL;    
+
+    always @(reset or hInc) begin
+        if(reset) begin
+            attrL = 0;
+        end else if (hInc) begin
+            case({cy[0], vram[0]})
+                2'b00 : attrL = atb[1:0];
+                2'b01 : attrL = atb[3:2];
+                2'b10 : attrL = atb[5:4];
+                2'b11 : attrL = atb[7:6];
+            endcase
+        end
+    end
+
+    reg [2:0] x, next_x, ctr;
+    wire [13:0] tileAddr, attrAddr;
+
+    reg [1:0] [15:0] bgSR, next_bgSR; 
+    reg [1:0] [7:0] attrSR;
+    reg [14:0] tvram, vram, next_tvram, next_vram;
+    reg w, next_w; 
+    wire hInc;
+    reg [8:0] renderLine, cycleNum;
+    wire [2:0] fineY; 
+    assign fineY = vram[14:12];
+    //TODO: needs more here, only true within certain range
+    assign hInc = (ctr == 3'd7) && ((cycleNum > 'd0 && cycleNum < 'd257) || (cycleNum > 'd320 && cycleNum < 'd337)) ;
+    
+
+    //pattern table Address for the BACKGROUND
+    assign ptAddr = { 1'b0,  PPUCTRL[4], bgA, &ctr[2:1], fineY};
+
+    assign sPT_base = PPUCTRL[3] ? 'h1000 : 0;
+    assign bPT_base = PPUCTRL[4] ? 'h1000 : 0; 
+    
+    assign cy = vram[9:5];
+    // This is doing the math to find the nametable address
+    // NESdev isn't great for really describing the nametable; each entry is a pointer into the background 
+    // as far as I can tell. Since each background is 
+    assign tileAddr = 'h2000 | (vram & 'h0fff); 
+    assign attrAddr = 'h23c0 | (vram & 'h0c00) | (vram[11:4]  & 8'h38 ) | (vram[4:2] & 3'h7);
+
+
+    
+
+    always @* begin
+        next_tvram = tvram;
+        next_vram = vram + hInc;
+        next_bgSR = bgSR;
+        next_x = x;
+        next_w = w;
+
+        //shift logic 
+
+
+        if ((cycleNum > 'd0 && cycleNum < 'd257) || (cycleNum > 'd320 && cycleNum < 'd337)) begin
+            next_bgSR[0] = {1'b0, bgSR[0][14:0]};
+            next_bgSR[1] = {1'b0, bgSR[1][14:0]};
+            if (hInc) begin
+                next_bgSR[0][15:8] = bgL;
+                next_bgSR[1][15:8] = vram2ppu_data;
+            end
+        end
+
+
+
+        case(PPUCTRL[1:0]) 
+            2'b00 : NT_base  = 'h2000;
+            2'b01 : NT_base  = 'h2400;
+            2'b10 : NT_base  = 'h2800;
+            2'b11 : NT_base  = 'h2c00;
+            default: NT_base = 'h2000;
+        endcase
+        
+        casez (ctr[2:1])
+            2'b00 : ppu2vram_addr = tileAddr;
+            2'b01 : ppu2vram_addr = attrAddr;
+            2'b1? : ppu2vram_addr = ptAddr; 
+        endcase
+
+        if (hInc && ((vram & 'h001f) == 'h001f)) begin
+            //If we go across a nametable, switch lower the nametable bit
+            next_vram = ((vram & ~'h001f) ^ 'h0400);
+        end
+
+        
+
+
+
+        if (cycleNum == 'd256) begin 
+            //If fine Y is less than 7, we increment fine Y bits
+            if((vram & 'h7000) != 'h7000) begin
+                next_vram = vram + 'h1000;
+            end else begin
+                next_vram = vram & ~'h7000;
+                if (cy == 'd29) begin
+                    next_vram[9:5] = 0;
+                    next_vram[11] = next_vram[11] ^ 1'b1;
+                end else if (cy == 'd31) begin
+                    next_vram[9:5] = 0;
+                end else begin 
+                    next_vram[9:5] = next_vram[9:5] + 1'b1;
+                end
+            end
+        end
+
+        if (cycleNum == 'd257) begin
+            next_vram[10] = tvram[10];
+            next_vram[4:0] = tvram[4:0];
+        end
+
+        
+        if (cycleNum > 'd279 && cycleNum < 'd305 && renderLine == 'd261) begin
+            next_vram[14:11] = tvram[14:11];
+            next_vram[9:5]   = tvram[9:5];
+        end
+
+
+
+
+        if (cpu2ppu_wr && cpu2ppu_addr == 'h2000) begin 
+            next_tvram = {tvram[14:12], cpu2ppu_data[1:0], tvram[9:0]};
+        end else if (w== 0 && cpu2ppu_wr && cpu2ppu_addr == 'h2005) begin
+            next_tvram = {tvram[14:5], cpu2ppu_data[7:3]};
+            next_x = cpu2ppu_data[2:0];
+            next_w = 1;
+        end else if (w==1 &&cpu2ppu_wr && cpu2ppu_addr == 'h2005) begin
+            next_tvram = {cpu2ppu_data[2:0], tvram[11:10], cpu2ppu_data[7:3], tvram[4:0]};
+            next_w = 0;
+        end else if (w== 0 && cpu2ppu_wr && cpu2ppu_addr == 'h2006) begin
+            next_tvram = {1'b0, cpu2ppu_data[5:0], tvram[7:0]};
+            next_w = 1;
+        end else if (w==1 &&cpu2ppu_wr && cpu2ppu_addr == 'h2006) begin
+            next_tvram = {tvram[14:8], cpu2ppu_data[7:0]};
+            next_vram = next_tvram;
+            next_w = 0;
+        end 
+
+
+
+
+
+    end
+
+    
+
+    always @(posedge clock )begin
+        if (reset) begin
+            PPUCTRL <= 0;
+            PPUMASK <= 0;
+            PPUSTAT <= 0; 
+            OAMADD  <= 0;
+            OAMDAT  <= 0;
+            PPUADD  <= 0;
+            PPUDAT  <= 0;
+            tvram   <= 0;
+            vram    <= 0;
+            renderLine <= 'd261;
+            cycleNum   <= 'd0;
+            ctr     <= 0;
+            w       <= 0;
+            bgSR    <= 0;
+            bgL     <= 0; 
+            ppu2cpu_data <=0;
+            ppu2cpu_addr <=0;
+            attrSR <= 0;
+        end else begin
+            PPUCTRL <= (cpu2ppu_wr && cpu2ppu_addr == 'h2000) ? cpu2ppu_data : PPUCTRL; 
+            PPUMASK <= (cpu2ppu_wr && cpu2ppu_addr == 'h2001) ? cpu2ppu_data : PPUMASK;
+            PPUSTAT <= (cpu2ppu_wr && cpu2ppu_addr == 'h2002) ? cpu2ppu_data : PPUSTAT; 
+            OAMADD  <= (cpu2ppu_wr && cpu2ppu_addr == 'h2003) ? cpu2ppu_data : OAMADD; 
+            OAMDAT  <= (cpu2ppu_wr && cpu2ppu_addr == 'h2004) ? cpu2ppu_data : OAMDAT; 
+            PPUADD  <= (cpu2ppu_wr && cpu2ppu_addr == 'h2006) ? cpu2ppu_data : PPUADD; 
+            PPUDAT  <= (cpu2ppu_wr && cpu2ppu_addr == 'h2007) ? cpu2ppu_data : PPUDAT; 
+            tvram   <= next_tvram;
+            vram    <= next_vram;
+            x       <= next_x;
+            w       <= next_w;
+            renderLine <= (cycleNum == 'd340) ? renderLine + 'd1 : renderLine;
+            cycleNum <= (cycleNum == 'd340) ? 0 : cycleNum + 'd1;
+            ctr      <= (cycleNum == 0) ? 0 : ctr + 'd1; 
+            bgSR    <= next_bgSR;
+            if (ctr == 'd1) begin
+                bgA <= vram2ppu_data; 
+            end
+            if (ctr == 'd3) begin
+               atb <=  vram2ppu_data; 
+            end 
+            if (ctr == 'd5) begin
+                bgL <= vram2ppu_data;
+            end
+            attrSR[0] <= {attrL[0], attrSR[0][7:1]};
+            attrSR[1] <= {attrL[1], attrSR[1][7:1]};
+            
+            
+        end 
+    end    
+
+endmodule
+
+
+
